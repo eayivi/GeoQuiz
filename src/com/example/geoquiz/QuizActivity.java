a0a77e2f01fd3bf3baba44c18d7efd5aa6f0bf0b
Enam Ayivi <enam.ayivi@utexas.edu>
2014-02-03 16:35:16 -0600

    Correct landscape issues fixes #1

--- src/com/example/geoquiz/QuizActivity.java
+++ src/com/example/geoquiz/QuizActivity.java
@@ -2,7 +2,7 @@ package com.example.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +12,8 @@ import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
-	private static final String TAG = "QuizActivity";
+	//private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private ImageView mNextButton;
@@ -50,7 +51,6 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreateBundle called");
         setContentView(R.layout.activity_quiz);
         
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -98,39 +98,48 @@ public class QuizActivity extends Activity {
 			}
         	
         });
-        
+        if ( savedInstanceState != null ) {
+        	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
+    }
+    
+    @Override
+    public void onSaveInstanceState( Bundle savedInstanceState ) {
+    	super.onSaveInstanceState( savedInstanceState );
+    	//Log.i(TAG, "onSaveInstanceState");
+    	savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
 
     @Override
     public void onStart() {
     	super.onStart();
-    	Log.d(TAG, "onStart() called");
+    	//Log.d(TAG, "onStart() called");
     }
     
     @Override
     public void onPause() {
     	super.onPause();
-    	Log.d(TAG, "onPause() called");
+    	//Log.d(TAG, "onPause() called");
     }
     
     @Override
     public void onResume(){
     	super.onResume();
-    	Log.d(TAG, "onResume() called");
+    	//Log.d(TAG, "onResume() called");
     }
     
     @Override
     public void onStop() {
     	super.onStop();
-    	Log.d(TAG, "onStop() called");
+    	//Log.d(TAG, "onStop() called");
     }
     
     @Override
     public void onDestroy() {
     	super.onDestroy();
-    	Log.d(TAG, "onDestroy() called");
+    	//Log.d(TAG, "onDestroy() called");
     }
     
     @Override
